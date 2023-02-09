0fd36c3903331eb60b89444dbe1e917e1a8be9f9
Erfan Ghorbani <erfan2ghorbani@gmail.com>
2023-02-09 13:55:17 +0330

    create init files of ZoneController

---
+++ app/Http/Controllers/Zone/ZoneController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\Zone;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ZoneRequests\StoreZoneRequest;
+use App\Http\Requests\ZoneRequests\UpdateZoneRequest;
+use App\Models\Zone;
+
+class ZoneController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\ZoneRequests\StoreZoneRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreZoneRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Zone  $zone
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Zone $zone)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Zone  $zone
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Zone $zone)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\ZoneRequests\UpdateZoneRequest  $request
+     * @param  \App\Models\Zone  $zone
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateZoneRequest $request, Zone $zone)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Zone  $zone
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Zone $zone)
+    {
+        //
+    }
+}
