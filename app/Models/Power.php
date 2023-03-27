127a56a8faf87b6f1c218cf8ec69623491eb49b1
Erfan Ghorbani <erfan2ghorbani@gmail.com>
2023-03-28 01:04:26 +0330

    power calculates and stores after each request from devices

---
+++ app/Models/Power.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Power extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'device_id',
+        'value',
+    ];
+}
